4e4e13d4bd8322afb1530e6a2be687e52a6a9d83
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2020-05-19 17:55:21 -0300

    Objetos para o Automato concluido
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

---
+++ ImplementacaoAFD/Automato.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ImplementacaoAFD
+{
+    public class Automato
+    {
+        public List<char> Alfabeto { get; set; }
+        public List<Estado> Estados { get; set; }
+        public List<Regra> Regras { get; set; }
+    }
+
+    public class Estado
+    {
+        public string Nome { get; set; }
+        public bool Inicial { get; set; }
+        public bool Final { get; set; }
+    }
+
+    public class Regra
+    {
+        public Estado EstadoAtual { get; set; }
+        public char Identificador { get; set; }
+        public Estado ProximoEstado { get; set; }
+    }
+}
