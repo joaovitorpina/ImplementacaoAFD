2d301e153bab4ecceba578091057cdf59411cbaa
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2020-05-19 22:20:32 -0300

    Finalizacao do programa principal
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- ImplementacaoAFD/Program.cs
+++ ImplementacaoAFD/Program.cs
@@ -7,27 +7,43 @@ namespace ImplementacaoAFD
         private static void Main(string[] args)
         {
             Automato automato = null;
-            Console.WriteLine("======MENU======");
-            Console.WriteLine("1- Criação AFD");
-            Console.WriteLine("2- Rodar Cadeia");
-            Console.WriteLine("3- Sair");
-            Console.WriteLine("================");
-            Console.WriteLine("Escolha uma opcão do menu:");
-            var menu = (Menu) Convert.ToInt32(Console.ReadLine());
-            switch (menu)
+            var sair = false;
+            do
             {
-                case Menu.CriacaoAFD:
-                    automato = new Automato();
-                    automato.CriarAutomato();
-                    automato.MostrarAutomato();
-                    break;
-                case Menu.RodarCadeia:
-                    break;
-                case Menu.Sair:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+                Console.WriteLine("======MENU======");
+                Console.WriteLine("1- Criação AFD");
+                Console.WriteLine("2- Rodar Cadeia");
+                Console.WriteLine("3- Sair");
+                Console.WriteLine("================");
+                Console.WriteLine("Escolha uma opcão do menu:");
+                var menu = (Menu) Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case Menu.CriacaoAFD:
+                        automato = new Automato();
+                        automato.CriarAutomato();
+                        automato.MostrarAutomato();
+                        break;
+                    case Menu.RodarCadeia:
+                        if (automato == null)
+                        {
+                            Console.WriteLine("Crie o automato antes de rodar uma cadeia");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Digite a cadeia a ser rodada no automato: ");
+                            var cadeia = Console.ReadLine()?.ToCharArray();
+                            automato.RodarCadeia(cadeia);
+                        }
+
+                        break;
+                    case Menu.Sair:
+                        sair = true;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            } while (!sair);
         }
     }
 
